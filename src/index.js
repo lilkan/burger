404eacb5bb8ecbe6690a7e8870240716710c5eb4
lilkan <doooomen@gmail.com>
2019-02-19 12:48:15 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const sum = require('./modules/sum.js');
+const guard = require('./modules/guard.js');
+const abc = require('jquery');
+
+console.log(sum(23,16));
+console.log(guard('German'));
+console.log(abc);
---
+++ src/modules/guard.js
@@ -0,0 +1,7 @@
+module.exports = function (name) { 
+    if (name == 'German'){
+        return 'hello, German!';
+    }else{
+        return 'Get out!';
+    }
+ };
